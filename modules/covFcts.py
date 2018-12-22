69198c0342cd04346dc1b650e0301bc9d284096d
Nicholas Krämer <nicholas.kraemer@t-online.de>
2018-12-22 10:56:48 +0100

    kernel becoms cov

--- modules/covFcts.py
+++ modules/covFcts.py
@@ -10,15 +10,15 @@ from __future__ import division
 import numpy as np
 import scipy.special
 
-def imqKernel(ptOne, ptTwo, imqPower = 1.0):
+def imqCov(ptOne, ptTwo, imqPower = 1.0):
 	distPts = np.linalg.norm(ptOne - ptTwo)
 	return 1.0 / np.sqrt((1 + distPts**2)**imqPower)
 
-def gaussKernel(ptOne, ptTwo, lengthScale = 1.0):
+def gaussCov(ptOne, ptTwo, lengthScale = 1.0):
 	distPts = np.linalg.norm(ptOne - ptTwo)
 	return np.exp(-distPts**2/(2.0*lengthScale**2))
 
-def maternKernel(ptOne, ptTwo, maternReg, maternScale = 1.0, maternCorrLength = 1.0):
+def maternCov(ptOne, ptTwo, maternReg, maternScale = 1.0, maternCorrLength = 1.0):
 	distPts = np.linalg.norm(ptOne - ptTwo)
 	if distPts <= 0:
 		return maternScale**2
@@ -27,11 +27,11 @@ def maternKernel(ptOne, ptTwo, maternReg, maternScale = 1.0, maternCorrLength = 
 		return maternScale**2 * 2**(1.0-maternReg) / scipy.special.gamma(maternReg) \
 			* scaledNormOfPts**(maternReg) * scipy.special.kv(maternReg, scaledNormOfPts)
 
-def expKernel(ptOne, ptTwo, lengthScale = 1.0):
+def expCov(ptOne, ptTwo, lengthScale = 1.0):
 	distPts = np.linalg.norm(ptOne - ptTwo)
 	return np.exp(-distPts/(lengthScale))
 
-def tpsKernel(ptOne, ptTwo):
+def tpsCov(ptOne, ptTwo):
 	distPts = np.linalg.norm(ptOne - ptTwo)
 	if distPts <= 0:
 		return 0
--- modules/covMtrcs.py
+++ modules/covMtrcs.py
@@ -12,45 +12,45 @@ from miscFcts import sph00, sph10, sph11, sph12, sph20, sph21, sph22, sph23, sph
 np.random.seed(15051994)
 np.set_printoptions(precision = 2)
 
-def buildKernelMtrxShift(ptSetOne, ptSetTwo, kernelFct, shiftPar):
+def buildCovMtrxShift(ptSetOne, ptSetTwo, covFct, shiftPar):
 	lenPtSetOne = len(ptSetOne)
 	lenPtSetTwo = len(ptSetTwo)
 	if lenPtSetOne != lenPtSetTwo:
 		print "The pointsets do not align... return 0"
 		return 0
-	kernelMtrx = np.zeros((lenPtSetOne, lenPtSetTwo))
+	covMtrx = np.zeros((lenPtSetOne, lenPtSetTwo))
 	for idx in range(lenPtSetOne):
 		for jdx in range(lenPtSetTwo):
-				kernelMtrx[idx,jdx] = kernelFct(ptSetOne[idx,:], ptSetTwo[jdx,:])
-	return kernelMtrx + shiftPar * np.identity(lenPtSetOne)
+				covMtrx[idx,jdx] = covFct(ptSetOne[idx,:], ptSetTwo[jdx,:])
+	return covMtrx + shiftPar * np.identity(lenPtSetOne)
 
-def buildKernelMtrx(ptSetOne, ptSetTwo, kernelFct):
+def buildCovMtrx(ptSetOne, ptSetTwo, covFct):
 	lenPtSetOne = len(ptSetOne)
 	lenPtSetTwo = len(ptSetTwo)
-	kernelMtrx = np.zeros((lenPtSetOne, lenPtSetTwo))
+	covMtrx = np.zeros((lenPtSetOne, lenPtSetTwo))
 	for idx in range(lenPtSetOne):
 		for jdx in range(lenPtSetTwo):
-			kernelMtrx[idx,jdx] = kernelFct(ptSetOne[idx,:], ptSetTwo[jdx,:])
-	return kernelMtrx
+			covMtrx[idx,jdx] = covFct(ptSetOne[idx,:], ptSetTwo[jdx,:])
+	return covMtrx
 
-def buildKernelMtrxCond(ptSetOne, ptSetTwo, kernelFct, polOrder = 1):
+def buildCovMtrxCond(ptSetOne, ptSetTwo, covFct, polOrder = 1):
 	lenPtSetOne = len(ptSetOne)
 	lenPtSetTwo = len(ptSetTwo)
 	dim = len(ptSetOne.T)
-	kernelMtrx = np.zeros((lenPtSetOne + dim *polOrder + 1, lenPtSetTwo + dim *polOrder + 1))
+	covMtrx = np.zeros((lenPtSetOne + dim *polOrder + 1, lenPtSetTwo + dim *polOrder + 1))
 	for idx in range(lenPtSetOne):
 		for jdx in range(lenPtSetTwo):
-			kernelMtrx[idx,jdx] = kernelFct(ptSetOne[idx,:], ptSetTwo[jdx,:])
+			covMtrx[idx,jdx] = covFct(ptSetOne[idx,:], ptSetTwo[jdx,:])
 		for jdx in range(polOrder):
-			kernelMtrx[idx, lenPtSetTwo] = 1
+			covMtrx[idx, lenPtSetTwo] = 1
 			for kdx in range(dim):
-				kernelMtrx[idx, lenPtSetTwo + dim*jdx + kdx + 1] = ptSetOne[idx,kdx]**(jdx+1)
+				covMtrx[idx, lenPtSetTwo + dim*jdx + kdx + 1] = ptSetOne[idx,kdx]**(jdx+1)
 	for idx in range(lenPtSetTwo):
-		kernelMtrx[lenPtSetOne, idx] = 1
+		covMtrx[lenPtSetOne, idx] = 1
 		for jdx in range(polOrder):
 			for kdx in range(dim):
-				kernelMtrx[lenPtSetOne + dim*jdx + kdx + 1, idx] = ptSetTwo[idx,kdx]**(jdx+1)
-	return kernelMtrx
+				covMtrx[lenPtSetOne + dim*jdx + kdx + 1, idx] = ptSetTwo[idx,kdx]**(jdx+1)
+	return covMtrx
 
 
 
