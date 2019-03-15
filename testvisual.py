0d6e62b5d76c5e896de3e70fc605f545a411ad10
Nicholas Krämer <nicholas.kraemer@t-online.de>
2019-03-15 10:32:01 +0100

    fig saving

--- testvisual.py
+++ testvisual.py
@@ -22,7 +22,7 @@ np.random.seed(1)
 
 # Set up problem and Gaussian process approximation
 num_obs = 8
-noise = 0.1
+noise = 0.
 data = ToyGPData(num_obs, noise)
 zero_mean = ZeroMean()
 cov_fct = MaternCov(2.)
@@ -33,12 +33,13 @@ cgp = ConditionedGaussianProcess(gp, data)
 
 num_pts = 200
 gpv = GPVisual(cgp, ctheme = "darkgreen")
-gpv.addanimation_samples()
+#gpv.addanimation_samples()
 gpv.addplot_mean()
 gpv.addplot_deviation()
 gpv.addplot_observations()
 plt.legend()
-gpv.anim.save("./figures/animations/gp_posterior_green.mp4", fps = 3, dpi = 250, writer="ffmpeg")#, fps=30, extra_args=['-vcodec', 'libx264'])
+gpv.fig.savefig("./figures/gpani.png", dpi = 250)
+#gpv.anim.save("./figures/animations/gp_posterior_green.mp4", fps = 3, dpi = 250, writer="ffmpeg")#, fps=30, extra_args=['-vcodec', 'libx264'])
 plt.show()
 
 
