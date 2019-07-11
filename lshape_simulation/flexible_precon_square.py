2d336ed096c11cfb60a720d3e875f9e47ba73adc
Nicholas Krämer <nicokraemer@gmx.net>
2019-07-11 15:54:47 +0200

    lshape update

--- lshape_simulation/flexible_precon_square.py
+++ lshape_simulation/flexible_precon_square.py
@@ -21,8 +21,9 @@ save_txt = True
 loc_radius = 12
 dim = 2
 
-num_pts = 65
-ptset = np.loadtxt("/home/kraemer/Programmieren/H2Lib/lshape/files/lmesh_N%u.txt"%num_pts)
+# 21, 65, 225, 833, 3201, ...
+num_pts = 3201
+ptset = np.loadtxt("/home/kraemer/Programmieren/H2Lib/lshape/files/mesh/lmesh_N%u.txt"%num_pts)
 
 
 
@@ -34,7 +35,7 @@ precon_vals, precon_rowidx, precon_colidx, num_neighb = LocalLagrange.precon(pts
 
 precon = scipy.sparse.coo_matrix((precon_vals, (precon_rowidx, precon_colidx)), shape=(num_pts + polblocksize, num_pts + polblocksize))
 
-print('\nInitialising\n\tN = %d\n\tn = %d'%(num_pts, num_neighb - polblocksize))
+print('\nInitialising\n\tN = %d\n\tn = %d'%(num_pts, num_neighb))
 print('Memory footprint of ptset:\n\t %.1f MB'%(ptset.nbytes/(1024**2)))
 print('Memory footprint  of sparse matrix:\n\t %.1f MB'%(sys.getsizeof(precon.data)/(1024**2)))
 print('Memory footprint of full matrix:\n\t %.1f MB'%((precon.toarray()).nbytes/(1024**2)))
@@ -47,23 +48,17 @@ print('Memory footprint of full matrix:\n\t %.1f MB'%((precon.toarray()).nbytes/
 if save_txt == True:
 	print('Saving')
 #	np.savetxt("precon_txt_tps_square/ptSet_Halton_N%d_n%d.txt"%(num_pts, num_neighb), ptset, fmt='%.17e')
-	np.savetxt("precon_lshape_tps/precon_val_N%d_n%d.txt"%(num_pts, num_neighb - polblocksize), precon_vals, fmt='%.17e')
-	np.savetxt("precon_lshape_tps/precon_row_N%d_n%d.txt"%(num_pts, num_neighb - polblocksize), precon_rowidx, fmt='%d')
-	np.savetxt("precon_lshape_tps/precon_col_N%d_n%d.txt"%(num_pts, num_neighb - polblocksize), precon_colidx, fmt='%d')
+	np.savetxt("/home/kraemer/Programmieren/H2Lib/lshape/files/precon/precon_val_N%d_n%d.txt"%(num_pts, num_neighb), precon_vals, fmt='%.17e')
+	np.savetxt("/home/kraemer/Programmieren/H2Lib/lshape/files/precon/precon_row_N%d_n%d.txt"%(num_pts, num_neighb), precon_rowidx, fmt='%d')
+	np.savetxt("/home/kraemer/Programmieren/H2Lib/lshape/files/precon/precon_col_N%d_n%d.txt"%(num_pts, num_neighb), precon_colidx, fmt='%d')
 	print('\tcomplete')
 print()
 
+if num_pts>15000:
+	sys.exit()
 
 P = precon.toarray()
 P[num_pts:(num_pts+3), num_pts:(num_pts+3)] = np.eye(3)
-
-
-
-
-
-
-
-
 covmat = TPS.fast_mtrx(ptset, ptset)
 cond_covmat = covmat.dot(P)
 #cond_covmat = cond_covmat[:num_pts, :num_pts]
@@ -72,6 +67,7 @@ cond_covmat = covmat.dot(P)
 #print("PTSET", ptset)
 #print("COVMAT", covmat)
 #print("PRECON", P)
+
 
 
 print('Computing GMRES')
